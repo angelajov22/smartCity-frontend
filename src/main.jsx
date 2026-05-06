20ba189583eb6478bca5f5bcf5eaa0a1432e5095
angelajov22 <jovanovskaangela77@gmail.com>
2026-05-06 23:24:23 +0200

    Fix imports and cleanup

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,8 @@
 import "./index.css";
-import App from "./App.jsx";
+import App from "./App";
 import "leaflet/dist/leaflet.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
