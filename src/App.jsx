4e66132c2add028db0e679b24442a8ec6136632c
unknown <velkovandrej819@gmail.com>
2026-05-10 01:05:55 +0200

    admin login

--- src/App.jsx
+++ src/App.jsx
@@ -6,21 +6,29 @@ import InstitutionsPage from "./pages/admin/InstitutionsPage";
 import AddInstitutionPage from "./pages/admin/AddInstitutionPage";
 import UsersPage from "./pages/admin/UsersPage";
 import SettingsPage from "./pages/admin/SettingsPage";
+import LoginPage from "./pages/admin/LoginPage";
+import { AuthProvider } from "./context/AuthContext";
+import ProtectedRoute from "./pages/admin/ProtectedRoute";
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/admin" element={<DashboardLayout />}>
-          <Route index element={<DashboardHomePage />} />
-          <Route path="institutions" element={<InstitutionsPage />} />
-          <Route path="institutions/add" element={<AddInstitutionPage />} />
-          <Route path="users" element={<UsersPage />} />
-          <Route path="settings" element={<SettingsPage />} />
-        </Route>
-        <Route path="*" element={<Navigate to="/" replace />} />
-      </Routes>
+      <AuthProvider>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/admin/login" element={<LoginPage />} />
+          <Route path="/admin" element={<DashboardLayout />}>
+            <Route element={<ProtectedRoute />}>
+              <Route index element={<DashboardHomePage />} />
+              <Route path="institutions" element={<InstitutionsPage />} />
+              <Route path="institutions/add" element={<AddInstitutionPage />} />
+              <Route path="users" element={<UsersPage />} />
+              <Route path="settings" element={<SettingsPage />} />
+            </Route>
+          </Route>
+          <Route path="*" element={<Navigate to="/" replace />} />
+        </Routes>
+      </AuthProvider>
     </BrowserRouter>
   );
 }
